ea62188749429cce87b8e0a0f8c8a3a3c09374f9
Atanas <atanas3angelov@gmail.com>
2018-12-18 14:33:17 +0200

    continuing the tutorial 2

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -5,7 +5,7 @@ class QuotesSpider (scrapy.Spider):
     name = "quotes"
     start_urls = [
         'http://quotes.toscrape.com/page/1/',
-        'http://quotes.toscrape.com/page/2/',
+        # 'http://quotes.toscrape.com/page/2/',
     ]
 
     # def start_requests(self):
@@ -29,4 +29,13 @@ class QuotesSpider (scrapy.Spider):
                 'author': quote.css("small.author::text").extract_first(),
                 'tags': quote.css("div.tags a.tag::text").extract(),
             }
+            # next_page = response.css("li.next a::attr(href)").extract_first()
+            # if next_page is not None:
+            #     # next_page = response.urljoin(next_page)
+            #     # yield scrapy.Request(next_page, callback=self.parse)
+            #     yield response.follow(next_page, callback=self.parse)
+            # for href in response.css("li.next a::attr(href)"):
+            #     yield response.follow(href, callback=self.parse)
+            for a in response.css("li.next a"):
+                yield response.follow(a, callback=self.parse)
 
